0d5e99394288e4abdea000c1d52ab2af8a6ecea3
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-09-05 23:40:03 -0400

    Add changes

---
+++ midterm/src/scripts/injectNav.ts
@@ -0,0 +1,13 @@
+function injectNav(navHtml: string) {
+  //Fetch the navbar content and insert it into the page
+  fetch(navHtml)
+    .then((response) => response.text())
+    .then((content) => {
+      document.getElementById(("navbar-placeholder".innerHTML = content));
+    })
+    .catch((error) => {
+      console.error("Could not fetch nav.", error);
+    });
+}
+
+injectNav("../");
